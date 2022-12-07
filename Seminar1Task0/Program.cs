f589660fb13fe71f2ecb3c492ee8754e26471fd6
nobilitas <ek.peregudova@gmail.com>
2022-12-07 23:45:02 +0300

    Seminar 1 task 0,1,3 added

---
+++ Seminar1Task0/Program.cs
@@ -0,0 +1,11 @@
+﻿string? num1line = Console.ReadLine();
+string? num2line = Console.ReadLine();
+
+if(num1line!=null&&num2line!=null)
+{
+    int num1 = int.Parse(num1line);
+    int num2 = int.Parse(num2line);
+    int result = num1*num2;
+
+    Console.WriteLine(result);
+}
---
+++ Seminar1Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿string? numberA = Console.ReadLine();
+if(numberA!=null)
+{
+    string[] weekDay = new string[7];
+    weekDay[0] = "Monday";
+    weekDay[1] = "Tuesday";
+    weekDay[2] = "Wednesday";
+    weekDay[3] = "Thursday";
+    weekDay[4] = "Friday";
+    weekDay[5] = "Saturday";
+    weekDay[6] = "Sunday";
+    int inputNumber = int.Parse(numberA);
+    string weekDayprint = weekDay[inputNumber - 1];
+    Console.WriteLine("Day of the week is");
+    Console.WriteLine(weekDayprint);
+}
