d9fae0d3c7193502069f7f130ff6e0249028e890
nobilitas <ek.peregudova@gmail.com>
2022-12-09 21:36:20 +0300

    Task 5,7 added webinar 1

---
+++ Seminar1Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿//Считываем число с консоли
+string? numN = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустые
+if(numN!=null)
+{
+    //Парсим введенное число 
+    int inputNumber = int.Parse(numN);
+
+    int startNumber = inputNumber*(-1);
+
+    //Выходное значение
+    string outLine = string.Empty;
+
+    while(startNumber < inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        startNumber++;
+    }
+    outLine = outLine + inputNumber;
+
+    //Выводим данные в консоль
+    Console.WriteLine(outLine);
+}
+    
+    
---
+++ Seminar1Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿//Считываем число с консоли
+string? numB = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустые
+if(numB!=null)
+{
+    //Парсим введенное число 
+    int inputNumber = int.Parse(numB);
+
+    int lastDigit = inputNumber % 10;
+
+    Console.WriteLine(lastDigit);
+}
+
+
